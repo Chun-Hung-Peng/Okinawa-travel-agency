d17bbafd89dee20f0dcba0bf5eb2412b6d7a2a72
Chun-Hung, Peng <tommy60022073@gmail.com>
2021-03-23 14:04:46 +0800

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let header = document.querySelector("header")
+let headerAnchor = document.querySelectorAll("header nav ul li a")
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset != 0) {
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.6)"
+        header.style.color = "white"
+        headerAnchor.forEach(a => {
+            a.style.color = "white"
+        })
+    }
+    else {
+        header.style = ""
+        headerAnchor.forEach(a => {
+            a.style.color = "#51AE9F"
+        })
+    }
+})
